cd51ddb266be0524183687839a62900c626b96bb
unknown <BT_3W2_03@BKTECH.LOCAL>
2024-12-13 15:31:30 -0500

    Started adding posts for specific users

--- scripts.js
+++ scripts.js
@@ -5,11 +5,34 @@ fetch('https://jsonplaceholder.typicode.com/users')
         $("#users-container").append(`
             <div id="user" user-id=${user.id}>
                 <div id="name-info">
-                    <h3 id="name">${user.name}</h1>
-                    <a id="username" href="user/${user.username}">@${user.username}</a>
+                    <h3 id="name">${user.name}</h3>
+                    <button id="user-link" onclick="openUser(${user.id})">@${user.username}</button>
                 </div>
             </div>`)
     });
-    
-    console.log(json)
 })
+
+async function openUser(userId) {
+    $("#selected-user-container").html(`        
+        <div id="selected-user-container">
+            <div id="selected-user-posts"></div>
+        </div>`)
+    fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+    .then(response => response.json())
+    .then(json => {
+         $("#selected-user-info").append(`
+             <div id="selected-user">
+                <h1 id="selected-name">${json.name}</h1>
+                <h3 id="selected-username">@${json.username}</h3>
+             </div>`)
+    });
+    fetch(`https://jsonplaceholder.typicode.com/posts`)
+    .then(response => response.json())
+    .then(json => {
+        json.filter(a => a.userId == userId).forEach(post => 
+            $("#selected-user-posts")
+            .append(`<h1 id="selected-post-title">${post.title}</h1>
+                   <h3 id="selected-post-body">@${post.body}</h3>
+                `)
+    )});
+}
